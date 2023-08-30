d0d4c4bdc5fc36980e947a46e1e1ba782cabfc71
Vijay <vijaynaga.0503@gmail.com>
2023-08-31 04:28:33 +0530

    first commit

---
+++ src/app/PageContent/page.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+
+export default function Home({ title, type, options, isActive, input, setInput, error, setError, currentPage }) {
+
+    const contentClasses = `content ${isActive ? 'active-content' : 'inactive-content'}`;
+    const alphabet = ['A', 'B', 'C', 'D', 'E'];
+
+    const [blinking, setBlinking] = useState(false);
+
+    const handleClick = () => {
+        if (!blinking) {
+            setBlinking(true);
+
+            setTimeout(() => {
+                setBlinking(false);
+            }, 900); // Adjust the duration as needed
+        }
+    };
+
+
+
+    if (type == "option") {
+        return (
+            <main className={`${contentClasses} flex h-screen  flex-col items-center justify-center  px-14 md:p-0`}>
+
+                <div className={`flex flex-col w-full md:w-3/4 lg:w-3/4 xl:w-3/5 text-left`}>
+                    <div className={`mb-3 flex items-center -ml-6 gap-1 text-2xl font-normal`}>
+                        <span className="text-xs text-blue-400 pr-px min-w-fit"> {currentPage + 1} -&gt;</span>{title}
+                    </div>
+
+
+                    {options.map((option, i) => {
+                        return (
+                            <div key={i}
+                                onClick={() => { handleClick(), setInput(option), setError(false) }}
+                                disabled={blinking}
+                                className={` mt-4 w-60 text-md cursor-pointer font-normal`}>
+                                <div className={`blinking-button ${(blinking && input === option) ? 'blink' : ''} flex mb-4 justify-between rounded-lg  px-2.5 py-2 transition-colors bg-gray-100 bg-opacity-10   hover:bg-gray-100 -700 hover:dark:bg-neutral-800 `}>
+                                    <span> <span className={`${(input === option) ? 'text-black bg-white' : ''} font-semibold text-sm border border-indigo-100 px-2 py-1 mr-3`}>{alphabet[i]}</span> {option} </span>
+                                    <span className={` ${(input === option) ? '' : 'hidden'} inline-block px-2 transition-transform  motion-reduce:transform-none`}>
+                                        ✓
+                                    </span>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                    {!error && <div className={`mt-4 flex flex-col items-start text-xl cursor-pointer font-semibold pb-4`}>
+                        <div className='flex mb-4 group rounded-lg border border-transparent px-2.5 py-2 transition-colors bg-gray-100 bg-opacity-10  hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:drk:bg-opacity-30'>
+                            <span >OK {' '}</span>
+                            <span className="inline-block pl-2 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                                ✓
+                            </span>
+                        </div>
+                    </div>}
+
+
+
+
+
+                    {error && <div className={`mt-4 flex flex-col items-start text-md cursor-pointer font-normal pb-4`}>
+                        <div className='flex mb-4 group rounded-lg border border-transparent px-2.5 py-2 transition-colors bg-gray-100 bg-opacity-10  hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:drk:bg-opacity-30'>
+                            <span className="Boundary-sc-__sc-184gmm6-0 dLVEpr"><svg height="24" viewBox="0 0 24 24" width="24"><path clipRule="evenodd" fill="red" d="M16.3361 17.9998L7.00279 18C5.49294 18 4.52754 16.391 5.23806 15.0588L9.90471 6.30882C10.6576 4.89706 12.6812 4.89706 13.4341 6.30881L18.1008 15.0586C18.8113 16.3908 17.8459 17.9998 16.3361 17.9998ZM11.6694 8.50003C12.2217 8.50003 12.6694 8.94774 12.6694 9.50003V11.5C12.6694 12.0523 12.2217 12.5 11.6694 12.5C11.1171 12.5 10.6694 12.0523 10.6694 11.5V9.50003C10.6694 8.94774 11.1171 8.50003 11.6694 8.50003ZM11.6694 16C12.2217 16 12.6694 15.5523 12.6694 15C12.6694 14.4477 12.2217 14 11.6694 14C11.1171 14 10.6694 14.4477 10.6694 15C10.6694 15.5523 11.1171 16 11.6694 16Z" fillRule="evenodd"></path></svg></span>
+                            <span className="inline-block pl-2 text-red-200 ">
+                                Please fill this in
+                            </span>
+                        </div>
+                    </div>}
+
+                </div>
+
+
+            </main >
+        )
+    }
+
+    return (
+        <main className={`${contentClasses} flex h-screen  flex-col items-center justify-center  px-14 md:p-0`}>
+
+            {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[140px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            </div> */}
+
+            <div className={`flex flex-col w-full md:w-3/4 lg:w-3/4 xl:w-3/5 text-left`}>
+                <div className={`mb-3 flex items-center -ml-6 gap-1 text-2xl font-normal`}>
+                    <span className="text-xs text-blue-400 pr-px min-w-fit"> {currentPage + 1} -&gt;</span>{title}
+                </div>
+                <div className="bg-transparent border-b-2 border-slate-200 pb-2">
+                    <input type="email" autoFocus className="text-xl focus:outline-none appearance-none bg-transparent" onChange={(e) => { setInput(e.target.value), setError(false) }} value={input} placeholder="Type your answer here..." />
+                </div>
+                {!error && <div className={`mt-4 flex flex-col items-start text-xl cursor-pointer font-semibold pb-4`}>
+                    <div className='flex mb-4 group rounded-lg border border-transparent px-2.5 py-2 transition-colors bg-gray-100 bg-opacity-10  hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:drk:bg-opacity-30'>
+                        <span >OK {' '}</span>
+                        <span className="inline-block pl-2 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            ✓
+                        </span>
+                    </div>
+                    <div className="text-xs">press Enter ↵</div>
+                </div>}
+
+                {error && <div className={`mt-4 flex flex-col items-start text-md cursor-pointer font-normal pb-4`}>
+                    <div className='flex mb-4 group rounded-lg border border-transparent px-2.5 py-2 transition-colors bg-gray-100 bg-opacity-10  hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:drk:bg-opacity-30'>
+                        <span className="Boundary-sc-__sc-184gmm6-0 dLVEpr"><svg height="24" viewBox="0 0 24 24" width="24"><path clipRule="evenodd" fill="red" d="M16.3361 17.9998L7.00279 18C5.49294 18 4.52754 16.391 5.23806 15.0588L9.90471 6.30882C10.6576 4.89706 12.6812 4.89706 13.4341 6.30881L18.1008 15.0586C18.8113 16.3908 17.8459 17.9998 16.3361 17.9998ZM11.6694 8.50003C12.2217 8.50003 12.6694 8.94774 12.6694 9.50003V11.5C12.6694 12.0523 12.2217 12.5 11.6694 12.5C11.1171 12.5 10.6694 12.0523 10.6694 11.5V9.50003C10.6694 8.94774 11.1171 8.50003 11.6694 8.50003ZM11.6694 16C12.2217 16 12.6694 15.5523 12.6694 15C12.6694 14.4477 12.2217 14 11.6694 14C11.1171 14 10.6694 14.4477 10.6694 15C10.6694 15.5523 11.1171 16 11.6694 16Z" fillRule="evenodd"></path></svg></span>
+                        <span className="inline-block pl-2 text-red-200 ">
+                            Please fill this in
+                        </span>
+                    </div>
+                </div>}
+
+            </div>
+
+
+        </main>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,112 @@
-import Image from 'next/image'
+'use client'
+import React, { useState } from 'react';
+import PageContent from '../app/PageContent/page';
 
 export default function Home() {
+
+  const pages = [
+    {
+      title: 'Email*',
+      titleValue: "",
+      type: 'question',
+      options: [],
+      content: 'This is the content of page 1.',
+    },
+    {
+      title: 'Full Name',
+      titleValue: "",
+      type: 'question',
+      options: [],
+      content: 'This is the content of page 2.',
+    },
+    {
+      title: 'Highest Level of EducationThis question is required.',
+      titleValue: "",
+      type: 'option',
+      options: ["Grade 12", "Bachelors", 'PhD', "Masters Degree", "Diploma"],
+      content: 'This is the content of page 2.',
+    }
+  ];
+
+
+  const [updatedPages, setUpdatedPages] = useState([...pages]);
+  const [input, setInput] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
+  const [error, setError] = useState('');
+
+
+  const handleLoadNextPage = () => {
+    if (!input) {
+      setError(true);
+      return console.log("error")
+    }
+
+    const updatedPagesCopy = [...updatedPages];
+    updatedPagesCopy[currentPage].titleValue = input;
+
+    setFadeIn(false); // Trigger fade-out effect
+
+    setTimeout(() => {
+      setUpdatedPages(updatedPagesCopy);
+      setCurrentPage((prevPage) => (prevPage + 1) % pages.length);
+      console.log(updatedPages[currentPage + 1].titleValue)
+      if (updatedPages[currentPage + 1].titleValue) {
+        setInput(updatedPages[currentPage + 1].titleValue)
+      } else {
+        setInput('');
+      }
+      setFadeIn(true); // Trigger fade-in effect
+    }, 240); // Adjust the delay to match your CSS transition duration
+    console.log(updatedPages, currentPage)
+  };
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div className="app">
+      <PageContent
+        title={pages[currentPage].title}
+        type={updatedPages[currentPage].type}
+        options={updatedPages[currentPage].options}
+        currentPage={currentPage}
+        setInput={setInput}
+        input={input}
+        isActive={fadeIn}
+        error={error}
+        setError={setError}
+      // content={pages[currentPage].content}
+      />
+      <button onClick={handleLoadNextPage}>Load Next Page</button>
+    </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    // <main className="flex h-screen   flex-col items-center justify-center p-10">
+    //   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[140px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    //   </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    //   <div className="flex flex-col  text-center w-3/5 ">
+    //     <h2 className={`mb-3 text-2xl font-semibold`}>
+    //       Customized SOP Generator
+    //     </h2>
+    //     <p className={`m-6  text-xl opacity-50`}>
+    //       Fill this questionnaire for the student. After submitting, you will receive an email at the email address that you have provided with a Statement of Purpose customized for you. You can use and modify that as per your needs.
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    //     </p>
+    //     <p className={`m-0  text-xl opacity-50`}>
+    //       If you would like to get it edited, reviewed, or drafted by our experts, you can get in touch with us: https://effizient-immigration-inc.square.site/s/shop
+    //     </p>
+    //     <div onClick={() => { window.location.href = '/emailPage' }} className={`group mt-3 w-full flex justify-center  text-2xl cursor-pointer font-semibold`}>
+    //       <div className='flex w-28 rounded-lg border border-transparent pl-2.5 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30'>
+    //         <span>Start {' '}</span>
+    //         <span className="inline-block  transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+    //           -&gt;
+    //         </span>
+    //       </div>
+    //     </div>
+    //   </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+
+    // </main>
   )
 }
